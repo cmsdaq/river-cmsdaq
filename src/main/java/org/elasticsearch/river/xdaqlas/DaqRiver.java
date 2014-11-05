8cd7d270cb455e00907e6a8bb30a97eb9cbfd488
Remi Mommsen <remigius.mommsen@cern.ch>
2014-11-05 11:21:33 +0100

    Split common river functionality into new DaqRiver abstract class.

---
+++ src/main/java/org/elasticsearch/river/xdaqlas/DaqRiver.java
@@ -0,0 +1,87 @@
+package org.elasticsearch.river.xdaqlas;
+
+import java.util.Map;
+
+import java.net.Proxy;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
+import org.elasticsearch.action.bulk.BulkProcessor;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.bulk.BulkItemResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.support.XContentMapValues;
+import org.elasticsearch.river.AbstractRiverComponent;
+import org.elasticsearch.river.River;
+import org.elasticsearch.river.RiverName;
+import org.elasticsearch.river.RiverSettings;
+
+
+public abstract class DaqRiver extends AbstractRiverComponent {
+
+  protected final Proxy lasProxy;
+  protected volatile BulkProcessor bulkProcessor;
+
+  @SuppressWarnings({"unchecked"})
+  @Inject
+  public DaqRiver(RiverName riverName, RiverSettings settings, Client client) {
+    super(riverName, settings);
+
+    Map<String, Object> rSettings = settings.settings();
+
+    // Create SOCKS proxy if requested
+    final Boolean useProxy = XContentMapValues.nodeBooleanValue(rSettings.get("useProxyForLAS"),false);
+    if (useProxy) {
+      final String hostname = XContentMapValues.nodeStringValue(rSettings.get("proxyHost"), "localhost");
+      final Integer port = XContentMapValues.nodeIntegerValue(rSettings.get("proxyPort"), 1080);
+      logger.info("Using SOCKS proxy "+hostname+":"+port);
+      SocketAddress addr = new
+        InetSocketAddress(hostname, port);
+      lasProxy = new Proxy(Proxy.Type.SOCKS, addr);
+    }
+    else {
+      lasProxy = null;
+    }
+
+    // Creating bulk processor
+    final Integer bulkActions = XContentMapValues.nodeIntegerValue(rSettings.get("bulkActions"), 100);
+    final Integer concurrentRequests = XContentMapValues.nodeIntegerValue(rSettings.get("concurrentRequests"), 10);
+    final Integer flushIntervalSeconds = XContentMapValues.nodeIntegerValue(rSettings.get("flushIntervalSeconds"), 10);
+
+    this.bulkProcessor = BulkProcessor.builder(client, new BulkProcessor.Listener() {
+        @Override
+        public void beforeBulk(long executionId, BulkRequest request) {
+          logger.debug("Going to execute new bulk composed of {} actions", request.numberOfActions());
+        }
+
+        @Override
+        public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+          logger.debug("Executed bulk composed of {} actions", request.numberOfActions());
+          if (response.hasFailures()) {
+            logger.warn("There was failures while executing bulk", response.buildFailureMessage());
+            if (logger.isDebugEnabled()) {
+              for (BulkItemResponse item : response.getItems()) {
+                if (item.isFailed()) {
+                  logger.debug("Error for {}/{}/{} for {} operation: {}", item.getIndex(),
+                               item.getType(), item.getId(), item.getOpType(), item.getFailureMessage());
+                }
+              }
+            }
+          }
+        }
+
+        @Override
+        public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+          logger.warn("Error executing bulk", failure);
+        }
+      })
+      .setBulkActions(bulkActions)
+      .setConcurrentRequests(concurrentRequests)
+      .setFlushInterval(TimeValue.timeValueSeconds(flushIntervalSeconds))
+      .build();
+  }
+}
--- src/main/java/org/elasticsearch/river/xdaqlas/XdaqLas.java
+++ src/main/java/org/elasticsearch/river/xdaqlas/XdaqLas.java
@@ -22,92 +22,37 @@ import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
 import org.json.simple.parser.JSONParser;
 
-import org.elasticsearch.action.bulk.BulkProcessor;
-import org.elasticsearch.action.bulk.BulkRequest;
-import org.elasticsearch.action.bulk.BulkResponse;
-import org.elasticsearch.action.bulk.BulkItemResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.Requests;
 import org.elasticsearch.common.inject.Inject;
-import org.elasticsearch.river.AbstractRiverComponent;
 import org.elasticsearch.river.River;
 import org.elasticsearch.river.RiverName;
 import org.elasticsearch.river.RiverSettings;
 import org.elasticsearch.common.xcontent.support.XContentMapValues;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
-import org.elasticsearch.common.unit.TimeValue;
 
 
-public class XdaqLas extends AbstractRiverComponent implements River {
+public class XdaqLas extends DaqRiver implements River {
 
   protected final String lasURL;
-  protected final Proxy lasProxy;
-  protected volatile BulkProcessor bulkProcessor;
   private volatile Thread thread;
 
   @SuppressWarnings({"unchecked"})
   @Inject
   public XdaqLas(RiverName riverName, RiverSettings settings, Client client) {
-    super(riverName, settings);
+    super(riverName, settings, client);
 
-    //RunRiver Settings
-    Map<String, Object> rSettings = settings.settings();
-    //lasURL = XContentMapValues.nodeStringValue(rSettings.get("lasURL"), "http://dvsrv-c2f36-09-01.cms:9941");
-    lasURL = XContentMapValues.nodeStringValue(rSettings.get("lasURL"), "http://pc-c2e11-18-01.cms:9941");
-
-    // Create SOCKS proxy if requested
-    final Boolean useProxy = XContentMapValues.nodeBooleanValue(rSettings.get("useProxyForLAS"),false);
-    if (useProxy) {
-      final String hostname = XContentMapValues.nodeStringValue(rSettings.get("proxyHost"), "localhost");
-      final Integer port = XContentMapValues.nodeIntegerValue(rSettings.get("proxyPort"), 1080);
-      logger.info("Using SOCKS proxy "+hostname+":"+port);
-      SocketAddress addr = new
-        InetSocketAddress(hostname, port);
-      lasProxy = new Proxy(Proxy.Type.SOCKS, addr);
-    }
-    else {
-      lasProxy = null;
-    }
-
-    // Creating bulk processor
-    this.bulkProcessor = BulkProcessor.builder(client, new BulkProcessor.Listener() {
-        @Override
-        public void beforeBulk(long executionId, BulkRequest request) {
-          logger.debug("Going to execute new bulk composed of {} actions", request.numberOfActions());
-        }
-
-        @Override
-        public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
-          logger.debug("Executed bulk composed of {} actions", request.numberOfActions());
-          if (response.hasFailures()) {
-            logger.warn("There was failures while executing bulk", response.buildFailureMessage());
-            if (logger.isDebugEnabled()) {
-              for (BulkItemResponse item : response.getItems()) {
-                if (item.isFailed()) {
-                  logger.debug("Error for {}/{}/{} for {} operation: {}", item.getIndex(),
-                               item.getType(), item.getId(), item.getOpType(), item.getFailureMessage());
-                }
-              }
-            }
-          }
-        }
-
-        @Override
-        public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
-          logger.warn("Error executing bulk", failure);
-        }
-      })
-      .setBulkActions(100)
-      .setConcurrentRequests(10)
-      .setFlushInterval(TimeValue.timeValueSeconds(10))
-      .build();
+    //lasURL = XContentMapValues.nodeStringValue(settings.settings().get("lasURL"), "http://dvsrv-c2f36-09-01.cms:9941");
+    lasURL = XContentMapValues.nodeStringValue(settings.settings().get("lasURL"), "http://pc-c2e11-18-01.cms:9941");
   }
 
   @Override
   public void start() {
     logger.info("Start retrieving data from "+lasURL);
-    thread = EsExecutors.daemonThreadFactory(settings.globalSettings(), "las_slurper").newThread(new Slurper(lasURL));
+    thread = EsExecutors.daemonThreadFactory(settings.globalSettings(), "las_slurper").newThread(
+      new Slurper(lasURL,this.lasProxy)
+    );
     thread.start();
   }
 
@@ -122,13 +67,16 @@ public class XdaqLas extends AbstractRiverComponent implements River {
     logger.info("Stopped retrieving data from "+lasURL);
   }
 
+
   private class Slurper implements Runnable {
     private final String lasURN;
+    private final Proxy lasProxy;
     private final JSONParser parser;
     private final DateFormat dateFormat;
 
-    private Slurper(String lasURL) {
+    private Slurper(String lasURL, Proxy lasProxy) {
       this.lasURN = lasURL+"/urn:xdaq-application:service=xmaslas2g";
+      this.lasProxy = lasProxy;
       this.parser = new JSONParser();
       this.dateFormat = new SimpleDateFormat("E, MMM dd yyyy HH:mm:ss z");
     }
